74fab8deaeef339767abae7b60980e5cf5422c44
iSpinHD <61386781+iSpinHD@users.noreply.github.com>
2020-10-31 19:18:44 +0100

    Oppgave 2

--- src/no/hvl/dat100/jplab11/oppgave2/Bilde.java
+++ src/no/hvl/dat100/jplab11/oppgave2/Bilde.java
@@ -4,35 +4,42 @@ import no.hvl.dat100.jplab11.common.TODO;
 
 public class Bilde extends Tekst {
 
-	// TODO - objekt variable
-	
+	private String URL;
+
 	public Bilde(int id, String bruker, String dato, String tekst, String url) {
-		throw new UnsupportedOperationException(TODO.constructor("Bilde"));
+		super(id, bruker, dato, tekst);
+		this.URL = url;
 	}
 
 	public Bilde(int id, String bruker, String dato, int likes, String tekst, String url) {
-		throw new UnsupportedOperationException(TODO.constructor("Bilde"));
+		super(id, bruker, dato, likes, tekst);
+		this.URL = url;
 	}
-	
+
 	public String getUrl() {
-		throw new UnsupportedOperationException(TODO.method());
+		return URL;
 
 	}
 
 	public void setUrl(String url) {
-		throw new UnsupportedOperationException(TODO.method());
+		this.URL = url;
 	}
 
 	@Override
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method ());
+		return "BILDE"
+		+ "\n" + getId()
+		+ "\n" + getBruker()
+		+ "\n" + getDato()
+		+ "\n" + getLikes()
+		+ "\n" + getTekst()
+		+ "\n" + getUrl() + "\n";
 
 	}
 
-	// Metoden nedenfor er kun for valgfri oppgave 6
 	public String toHTML() {
-		
+
 		throw new UnsupportedOperationException(TODO.method());
-				
+
 	}
 }
